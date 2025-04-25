0c0e63cc84262b4fc4b4d31cb7a358e29ad4acc0
bmink <barnamink@gmail.com>
2025-04-24 19:40:31 -0700

    First commit

---
+++ main/font.h
@@ -0,0 +1,18 @@
+#ifndef FONT_H
+#define FONT_H
+
+typedef struct font_char {
+	uint8_t		fc_width;
+	uint32_t	fc_bitmap_offs;
+	uint8_t		fc_bitmap_siz;
+} font_char_t;
+
+typedef struct font {
+	uint8_t		fo_line_height;
+	uint8_t		fo_ascii_min;
+	uint8_t		fo_ascii_max;
+	font_char_t	*fo_chars;
+	uint8_t		*fo_bitmap;
+} font_t;
+
+#endif
